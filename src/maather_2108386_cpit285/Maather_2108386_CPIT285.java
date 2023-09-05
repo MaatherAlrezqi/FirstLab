7b87d608b6c21b7776c58b377011a1de92933e14
Maather Alrezqi <maatheralrezqi@gmail.com>
2023-09-06 00:41:53 +0300

    First commit

---
+++ src/maather_2108386_cpit285/Maather_2108386_CPIT285.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package maather_2108386_cpit285;
+
+/**
+ *
+ * @author maath
+ */
+public class Maather_2108386_CPIT285 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
